7acdacb6bfc9eba1078880477965b7c30b52808f
Daniel Lin <87847438+cryogopher@users.noreply.github.com>
2026-03-17 08:55:18 -0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
+#include <driver/i2s.h> //for the ics43434 mic
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 2
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  Serial.println("ESP32 Initialized in PlatformIO!");
+  Serial.println("\n--- NEW UPLOAD V1.0 EXECUTING ---");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("LED ON");
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("LED OFF");
+  delay(1000);
 }
